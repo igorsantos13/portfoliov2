0a1002f667b64251d4d823a71d7b1fa9a4912780
Igor Santos <igorsantos9513@outlook.com>
2023-11-27 20:21:25 -0300

    fmc

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -7,7 +7,7 @@ function Experience() {
     <main className='main'>
       <div className='card-container'>
           <Card link='https://igorsantos13.github.io/tomodoro/' codeLink='https://github.com/igorsantos13/tomodoro' name='Tomodoro' description='Productivity app, aligning your tasks with your time.'/>
-          <Card link='https://igorsantos13.github.io/pokedex/' codeLink='https://github.com/igorsantos13/pokedex' name='Pokédex' description='A fun app built with PokeAPI. Get to know your favorite pokemon better!'/>
+          <Card link='https://www.forkcommitmerge.io/' codeLink='https://github.com/fork-commit-merge/fork-commit-merge' name='Fork Commit Merge' description=''/>
           <Card link='https://blog-mockup-nine.vercel.app/' codeLink='https://github.com/igorsantos13/codelandia-blog' name='Blog Mockup' description='Mockup website for a blog'/>
           <Card link='https://jordan-shoes-jet.vercel.app/' codeLink='https://github.com/igorsantos13/codelandia-jordan' name='Jordan Shoes' description='Front-End website for a shoes store'/>
       </div>
